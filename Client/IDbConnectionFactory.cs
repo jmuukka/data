75faae66c8480bd32314af0510bbac81ff78650a
BLACK\Jarmo <jarmo.muukka@gmail.com>
2018-03-07 21:28:25 +0200

    Client 1.0.0

---
+++ Client/IDbConnectionFactory.cs
@@ -0,0 +1,16 @@
+﻿using System.Data;
+
+namespace Mutex.Data
+{
+    /// <summary>
+    /// Provides an interface for creating IDbConnection objects.
+    /// </summary>
+    public interface IDbConnectionFactory
+    {
+        /// <summary>
+        /// Creates an object which implements IDbConnection.
+        /// </summary>
+        /// <returns>An IDbConnection object.</returns>
+        IDbConnection Create();
+    }
+}

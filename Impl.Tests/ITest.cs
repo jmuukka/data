3ea2f679a583bc59de8a7f8590f0f812269a4c26
BLACK\Jarmo <jarmo.muukka@gmail.com>
2018-03-14 20:25:15 +0200

    Impl Tests

---
+++ Impl.Tests/ITest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mutex.Data.Impl.Tests
+{
+    public interface ITest
+    {
+        int? Id { get; }
+
+        string RequireString20 { get; set; }
+
+        string NullableString10 { get; set; }
+
+        DateTime? RequireDateTime { get; set; }
+
+        DateTime? NullableDateTime { get; set; }
+
+        byte[] NullableBytes { get; set; }
+
+        object NullableVariant { get; set; }
+    }
+}
---
+++ Impl.Tests/Test.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mutex.Data.Impl.Tests
+{
+    public class Test : ITest
+    {
+        public int? Id { get; private set; }
+
+        public string RequireString20 { get; set; }
+
+        public string NullableString10 { get; set; }
+
+        public DateTime? RequireDateTime { get; set; }
+
+        public DateTime? NullableDateTime { get; set; }
+
+        public byte[] NullableBytes { get; set; }
+
+        public object NullableVariant { get; set; }
+    }
+}
